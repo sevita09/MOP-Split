38f0963a0f05adf917beeb4fd2f79035fdd04244
svita <seba.vita@gmail.com>
2026-08-02 01:18:57 -0300

    Agrega la paleta y los estilos base de la app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './tema.css';
 
 const raiz = document.getElementById('raiz');
 if (!raiz) throw new Error('Falta el nodo #raiz en index.html');
